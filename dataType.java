c7c66d814d1f8da9da4048e51f990c54f1702886
AdityaRaj81 <Connect2RajAditya@gmail.com>
2024-12-22 23:58:49 +0530

    test

--- dataType.java
+++ dataType.java
@@ -2,12 +2,12 @@ public class dataType {
     public static void main(String[] args) {
         boolean isJavaFun = true;
         boolean isFishTasty = false;
-        System.out.println(isJavaFun);
-        System.out.println(isFishTasty);
+        System.out.println("Is Java fun? " + isJavaFun);
+        System.out.println("Is fish tasty? " + isFishTasty);
 
+        // Boolean values are mostly used for conditional statements
         int x = 10;
         int y = 9;
-        System.out.println(x>y); // returns true, because 10 is higher than 9
-
+        System.out.println("Is x greater than y? " + (x > y)); // returns true, because 10 is higher than 9
     }
 }
