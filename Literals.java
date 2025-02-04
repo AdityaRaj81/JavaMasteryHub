0d7e46dc35faf208c591d07d066f8cd9f4d30948
AdityaRaj81 <Connect2RajAditya@gmail.com>
2025-02-04 20:16:48 +0530

    add Literals.java

---
+++ Literals.java
@@ -0,0 +1,25 @@
+public class Literals {
+    public static void main (String arge[]){
+        // literals
+        int num1 =  0b101; //Binary literal
+        System.out.println(num1);
+
+        int num11 = 0x7E; //Hexadecimal literal
+        System.out.println(num11);
+
+        double num2 = 56; //Decimal literal
+        System.out.println(num2);
+
+        double num22 = 12e10; //Scientific literal
+        System.out.println(num22);
+
+        float num3 = 56.7f; //Float literal
+        System.out.println(num3);
+
+        char c = 'a';    //literals
+        System.out.println(c);
+        c++;
+        System.out.println(c);
+
+    }
+}
